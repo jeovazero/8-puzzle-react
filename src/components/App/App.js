d1a5ec1d1646f796382ae37ef2609805da4a6e45
jeovazero <jeovapereira2000@live.com>
2018-10-25 12:50:09 -0300

    Created App component

---
+++ src/components/App/App.js
@@ -0,0 +1,76 @@
+import React from "react";
+import moves from "../../actions/moves";
+import Square from "../Squares/Square";
+import styled from "styled-components";
+import { connect } from 'react-redux';
+
+const CODE_KEY = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+}
+const state = [[1]];
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.keyHandle = this.keyHandle.bind(this);
+    console.log(props)
+  }
+  keyHandle(event){
+    switch(event.keyCode){
+      case CODE_KEY.LEFT:
+        store.dispatch(this.goLeft);
+        break;
+      case CODE_KEY.UP:
+        store.dispatch(this.goUp);
+        break;
+      case CODE_KEY.RIGHT:
+        store.dispatch(this.goRight);
+        break;
+      case CODE_KEY.DOWN:
+        store.dispatch(this.goDown);
+        break;
+      default: console.log('X')
+    }
+  }
+  componentWillMount(){
+    document.addEventListener("keypress", this.keyHandle)
+  }
+  render(){
+    return <div className={this.props.className} >
+      { 
+        this.props.grid.map( ({digit}) => 
+          <Square key={digit}>{ digit != 0 ? digit : '-' }</Square>
+        )
+      }
+    </div>
+  }
+};
+
+const mapStateToProps = (state) => ({
+  grid: state.moves.grid,
+  pos_blank: state.moves.pos_blank
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  goLeft: () => dispatch(moves.goLeft),
+  goRight: () => dispatch(moves.goRight),
+  goDown: () => dispatch(moves.goDown),
+  goUp: () => dispatch(moves.goUp)
+})
+
+const _App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+const AppStyled = styled(_App)`
+  width: 300px;
+  padding: 20px;
+  div{
+    float:left;
+  }
+`;
+
+export default AppStyled;
--- src/index.js
+++ src/index.js
@@ -1,79 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Square from "./components/Squares/Square";
-import styled from "styled-components";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from "./reducers";
-import move from "./actions/move";
+import App from "./components/App/App";
 
 const store = createStore(rootReducer);
 
-const state = {
-  grid:[
-    [1, 2, 3],
-    [4, 5, 0],
-    [7, 8, 6]
-  ],
-  pos_blank: {
-    x: 2, y: 1
-  }
-}
-
-const CODE_KEY = {
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40
-}
-
-class Box extends React.Component{
-  constructor(props){
-    super(props);
-    this.keyHandle = this.keyHandle.bind(this);
-  }
-  keyHandle(event){
-    switch(event.keyCode){
-      case CODE_KEY.LEFT:
-        store.dispatch(move.goLeft);
-        break;
-      case CODE_KEY.UP:
-        store.dispatch(move.goUp);
-        break;
-      case CODE_KEY.RIGHT:
-        store.dispatch(move.goRight);
-        break;
-      case CODE_KEY.DOWN:
-        store.dispatch(move.goDown);
-        break;
-      default: console.log('X')
-    }
-  }
-  componentWillMount(){
-    document.addEventListener("keypress", this.keyHandle)
-  }
-  render(){
-    return <div className={this.props.className} >
-      { 
-        state.grid.reduce((a, b) => a.concat(b), []).map( i => 
-          <Square key={i}>{ i != 0 ? i : '-' }</Square>
-        )
-      }
-    </div>
-  }
-};
-
-const BoxStyled = styled(Box)`
-  width: 300px;
-  padding: 20px;
-  div{
-    float:left;
-  }
-`;
+console.log(store.getState())
 
 ReactDOM.render(
   <Provider store={store}>
-    <BoxStyled />
+    <App />
   </Provider>,
   document.getElementById("hue")
 );
