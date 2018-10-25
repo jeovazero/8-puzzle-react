a78f764156da9162a95b8c5a0a47a5954ca6130b
jeovazero <jeovapereira2000@live.com>
2018-10-25 08:12:15 -0300

    added styled-components :purple_heart:

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
-const Hue = () => {
-  return <div>HUE HUE</div>
-};
+const Hue = styled.div`
+  border: 2px solid tomato;
+  padding: 10px 20px;
+  background-color: #e30372;
+  color: white;
+  font-weight: bolder;
+  font-family: sans-serif;
+`;
 
-ReactDOM.render(<Hue />, document.getElementById("hue"));
+ReactDOM.render(<Hue>HUE HUE beauty!</Hue>, document.getElementById("hue"));
