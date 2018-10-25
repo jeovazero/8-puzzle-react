5f927c4253a8d4e09615324d9a46fd6eb62d12ea
jeovazero <jeovapereira2000@live.com>
2018-10-25 09:37:37 -0300

    initial grid

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from "react-dom";
 import Square from "./Squares/Square";
 import styled from "styled-components";
 
+const state = [
+  [1, 2, 3],
+  [4, 5, 0],
+  [7, 8, 6]
+]
+
 const Box = ({className}) => (
   <div className={className} >
-    <Square> 5 </Square>
+    { 
+      state.reduce((a, b) => a.concat(b), []).map( i => 
+        <Square>{ i != 0 ? i : '-' }</Square>
+      )
+    }
   </div>
 );
 
 const BoxStyled = styled(Box)`
-  width: 200px;
-  height: 200px;
+  width: 300px;
+  padding: 20px;
+  div{
+    float:left;
+  }
 `;
 
 ReactDOM.render(<BoxStyled />, document.getElementById("hue"));
