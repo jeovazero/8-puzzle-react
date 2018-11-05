913d0a37cbfd8d4bf92c8dfc15f9427350d85413
jeovazero <jeovapereira2000@live.com>
2018-11-04 22:37:16 -0200

    components reorganizeds

---
+++ src/components/Button/ButtonSet.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ButtonSet = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+  div{
+    margin: 0 10px;
+  }
+`;
+
+export default ButtonSet;
---
+++ src/components/Title/Title.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { STYLE } from "@/styles.js";
+
+const Title = ({className, img, text}) => (
+  <div className={className}>
+    { img && <img src={img}></img> }
+    { text && <span>{text}</span> }
+  </div>
+)
+
+const StyledTitle = styled(Title)`
+  width: 250px;
+  img{
+    width: 100%;
+  }
+  span{
+    width: 100%;
+    display: block;
+    height: 2em;
+    margin: 10px 0 5px;
+    color: white;
+    font-family: ${ STYLE.fontFamily }, sans-serif;
+  }
+  text-align: center;
+  margin: 20px 0 10px;
+`;
+
+export default StyledTitle;
