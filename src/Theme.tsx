f6bc511464a603006d94b20769561e327e0e7833
jeovazero <contato@jeova.ninja>
2021-05-21 01:51:10 -0300

    feat: add more themes

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,7 +1,56 @@
 import styled from 'styled-components'
+import { FONT_FAMILY, THEME } from './tokens'
+
+const themeCss = Object
+  .entries(THEME)
+  .map(([key, values]) => `
+    &[data-theme="${key}"] {
+      --primary: ${values.primary};
+      --primaryLight: ${values.primaryLight};
+    }`)
+
+const themeOptionCss = Object
+  .entries(THEME)
+  .map(([key, values]) => `
+    &[data-color="${key}"] {
+      background-color: ${values.primary};
+      :after {
+        background-color: ${values.primaryLight};
+      }
+    }`)
+
+export const ThemeOption = styled.div`
+  width: 24px;
+  height: 24px;
+  margin: 4px;
+  border-radius: 50%;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transform: rotate(-15deg);
+  transition: transform 0.25s ease;
+  transform-origin: 50% 50%;
+  border: 3px solid white;
+  ${themeOptionCss};
+  :hover {
+    transform: scale(1.25) translateY(-6px);
+  }
+  :after {
+    position: absolute;
+    content: '';
+    display: block;
+    width: 24px;
+    height: 12px;
+    bottom: 0;
+  }
+  &[data-selected] {
+    box-shadow: -1px 3px 3px 0px #aaa;
+  }
+`
 
 export const Theme = styled.main`
   --primary: #560F80;
   --primaryLight: #781AAE;
-  --fontFamily: 'Pattaya', sans-serif;
+  --fontFamily: ${FONT_FAMILY};
+  ${themeCss}
 `
