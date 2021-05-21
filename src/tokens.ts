d72a3b1200e82b7426631e1aacf2ecb2fd761880
jeovazero <contato@jeova.ninja>
2021-05-21 01:50:02 -0300

    chore: add design tokens

---
+++ src/tokens.ts
@@ -0,0 +1,26 @@
+export const FONT_FAMILY = '\'Pattaya\', sans-serif'
+
+export const THEME = {
+  purple: {
+    primary: '#560F80',
+    primaryLight: '#781AAE'
+  },
+  red: {
+    primary: 'darkred',
+    primaryLight: 'red'
+  },
+  orange: {
+    primary: 'darkorange',
+    primaryLight: 'orange'
+  },
+  blue: {
+    primary: 'darkblue',
+    primaryLight: 'blue'
+  }
+} as const
+
+const isThemeColor = (t: string): t is ThemeColor => t in THEME
+
+export const THEME_COLORS = Object.keys(THEME).filter(isThemeColor)
+
+export type ThemeColor = keyof typeof THEME
